5a5dfb13dda43b15990d322f0667516001f0a89b
DorDaniel1989 <danielovelleiro@outlook.es>
2022-01-29 17:48:51 +0100

    Agregar archivos de proyecto.

---
+++ WebApiTiempoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+
+public class WebApiTiempoContext : DbContext
+{
+
+    public WebApiTiempoContext(DbContextOptions<WebApiTiempoContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<TiempoItem> TiempoItem { get; set; }
+    public string connString { get; private set; }
+    public WebApiTiempoContext()
+    {
+       
+       // connString = $"Server=(localdb)\\mssqllocaldb;Database=WebApiTiempoContext;Trusted_Connection=True;MultipleActiveResultSets=true";
+         connString = $"Server=185.60.40.210\\SQLEXPRESS,58015;Database=DB09Dani;User Id=sa;Password=Pa88word";
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(connString);
+    }
+}
+
